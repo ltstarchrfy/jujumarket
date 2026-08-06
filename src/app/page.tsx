5ee5573a9226658f73287d53d49c14388f8131fb
Z User <z@container>
2026-08-06 06:21:04 +0000

    d05a0677-4e48-4372-b18d-21ad55669ead

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2796,7 +2796,7 @@ export default function AstuteApp() {
               {/* QR Code Image */}
               <div className="flex items-center justify-center px-4 pb-4">
                 <img 
-                  src="/QRIS.png" 
+                  src="/QRIS.png?v=20260806" 
                   alt="QRIS Payment Code" 
                   className="w-full max-w-[360px] h-auto block rounded-xl"
                   style={{ aspectRatio: "3/4" }}
