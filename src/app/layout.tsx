70accd5d1d73349cb6cd3bbbccc07e20e3b50b33
Z User <z@container>
2026-09-24 19:01:59 +0000

    Change website title to JUJU MARKET

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 
 export const metadata: Metadata = {
-  title: "LINK DOWNLOAD BY JUJU - PRIVATE SERVER ASTUTE",
+  title: "JUJU MARKET",
   description:
     "Reseller private server terbaru dengan fitur lengkap dan anti-ban protection. Download ASTUTE sekarang!",
   keywords: ["ASTUTE", "Reseller", "Private Server", "Mod", "Android"],
